d8f1dd36c6584070b5f6230f14055b242041b4a1
OlenaLotos <olenameshanych@gmail.com>
2022-09-21 20:07:38 +0300

    edit reset in adding

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,12 +4,11 @@ import {
   useAddContactsMutation,
 } from '../../Redux/сontactsApi';
 import { Form, Label, Input, Button } from './ContactForm.styled';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export default function ContactForm() {
   const { data: contacts, refetch } = useGetContactsQuery();
   const [addContact, { isLoading }] = useAddContactsMutation();
-  // const [name, setName] = useState('');
-  // const [number, setNumber] = useState('');
 
   const [contact, setContact] = useState({
     name: '',
@@ -21,38 +20,24 @@ export default function ContactForm() {
     const form = event.target;
     const { name, number } = form.elements;
     const alreadyAdded = await contacts.some(obj => obj.name === name.value);
+
     alreadyAdded
-      ? alert(`Contact ${name} is already in your contacts`)
+      ? alert(`Contact is already in your contacts`)
       : await addContact({ name: name.value, number: number.value });
+    console.log(name.value);
+    console.log(number.value);
+    Notify.success(`Contact successfully added`);
     reset();
     refetch();
-  };
-
-  const reset = () => {
-    // setName('');
-    // setNumber('');
-    setContact('');
   };
 
   const handleChange = evt => {
     setContact(prev => ({ ...prev, [evt.target.name]: evt.target.value }));
   };
 
-  // const handleChange = evt => {
-  //   const { name, value } = evt.currentTarget;
-  //   switch (name) {
-  //     case 'name':
-  //       setName(value);
-  //       break;
-
-  //     case 'number':
-  //       setNumber(value);
-  //       break;
-
-  //     default:
-  //       break;
-  //   }
-  // };
+  const reset = () => {
+    setContact({ name: '', number: '' });
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -86,81 +71,3 @@ export default function ContactForm() {
     </Form>
   );
 }
-
-// import { useState } from 'react';
-// import {
-//   useGetContactsQuery,
-//   useAddContactsMutation,
-// } from '../../Redux/сontactsApi';
-// import { Form, Label, Input, Button } from './ContactForm.styled';
-
-// export default function ContactForm() {
-//   const { data: contacts, refetch } = useGetContactsQuery();
-//   const [addContact, { isLoading }] = useAddContactsMutation();
-//   const [name, setName] = useState('');
-//   const [number, setNumber] = useState('');
-
-//   const handleSubmit = event => {
-//     event.preventDefault();
-//     if (contacts.find(contact => contact.name === name)) {
-//       return alert(`${name} is already in your contacts list`);
-//     }
-
-//     addContact({ name: name, number: number });
-//     reset();
-//     refetch();
-//   };
-
-//   const reset = () => {
-//     setName('');
-//     setNumber('');
-//   };
-
-//   const handleChange = evt => {
-//     const { name, value } = evt.currentTarget;
-//     switch (name) {
-//       case 'name':
-//         setName(value);
-//         break;
-
-//       case 'number':
-//         setNumber(value);
-//         break;
-
-//       default:
-//         break;
-//     }
-//   };
-
-//   return (
-//     <Form onSubmit={handleSubmit}>
-//       <Label>Name</Label>
-//       <Input
-//         type="text"
-//         name="name"
-//         value={name}
-//         placeholder="Enter name"
-//         onChange={handleChange}
-//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//         required
-//       />
-
-//       <Label>Number</Label>
-//       <Input
-//         type="tel"
-//         name="number"
-//         value={number}
-//         placeholder="Enter number"
-//         onChange={handleChange}
-//         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//         required
-//       />
-
-//       <Button type="submit" disabled={isLoading}>
-//         Add new contact
-//       </Button>
-//     </Form>
-//   );
-// }
--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDeleteContactsMutation } from '../../Redux/сontactsApi';
 import { ItemLi, Button, Link } from './ContactItem.styled';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export const ContactItem = ({ id, name, number }) => {
   const [deleteContact, { isLoading }] = useDeleteContactsMutation();
 
   const onDeleteContact = id => {
     deleteContact(id);
+    Notify.success(`Contact successfully removed`);
   };
   return (
     <ItemLi key={id}>
